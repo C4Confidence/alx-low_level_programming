89400a4ef86a5bd618bf359863720a5ba7d664a5
C4Confidence <iamconfidence5@gmail.com>
2023-11-27 06:09:18 -0800

    Tread Lightly, She is near

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
+
+#endif
